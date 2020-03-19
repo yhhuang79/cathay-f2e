8c676318f2e1020c76db2b25c560a8acb45ecebc
Yu-Hsiang Huang <yhhuang79@gmail.com>
2020-03-20 05:16:38 +0800

    Updates

---
+++ precache-manifest.027ece6c65753f90083b557246368964.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64751fcb4686d1e487f148d308ff9e1a",
+    "url": "/cathay-f2e/index.html"
+  },
+  {
+    "revision": "d698af2587cc8f08e8b1",
+    "url": "/cathay-f2e/static/css/main.60f9e1d7.chunk.css"
+  },
+  {
+    "revision": "43a06104fb8988553222",
+    "url": "/cathay-f2e/static/js/2.08e39df6.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/cathay-f2e/static/js/2.08e39df6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d698af2587cc8f08e8b1",
+    "url": "/cathay-f2e/static/js/main.193626d0.chunk.js"
+  },
+  {
+    "revision": "2a3daa94ef5382775315",
+    "url": "/cathay-f2e/static/js/runtime-main.adc1a9aa.js"
+  }
+]);
